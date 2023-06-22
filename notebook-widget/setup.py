cfa6124d2bdc0e50de2de109508a757c761f5bba
Jay Wang <jay@zijie.wang>
2023-06-22 08:23:28 -0400

    Bump version: 0.1.1 → 0.1.2

--- notebook-widget/setup.py
+++ notebook-widget/setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/poloclub/wizmap",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
